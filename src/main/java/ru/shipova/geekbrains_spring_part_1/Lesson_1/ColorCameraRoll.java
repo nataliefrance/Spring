7be0a33db4ce3f22aa689be04afb3ec44967d3e6
Natalia Shipova <nataliefrance@mail.ru>
2019-10-15 20:16:24 +0300

    Курс GeekBrains Soring Framework часть 1, урок 1: Конфигурирование с использованием аннотаций.

--- src/main/java/ru/shipova/geekbrains_spring_part_1/Lesson_1/ColorCameraRoll.java
+++ src/main/java/ru/shipova/geekbrains_spring_part_1/Lesson_1/ColorCameraRoll.java
@@ -1,5 +1,8 @@
 package ru.shipova.geekbrains_spring_part_1.Lesson_1;
 
+import org.springframework.stereotype.Component;
+
+@Component("cameraRoll")
 public class ColorCameraRoll implements CameraRoll {
     public void processing() {
         System.out.println("-1 цветной кадр");
